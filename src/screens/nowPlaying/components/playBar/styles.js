eaa677ea2d0f11f8ac60d47c92b1d069f9f4b095
Nycollas <nycollas.duaarte@hotmail.com>
2020-05-31 20:42:19 -0300

    Create styles.js

---
+++ src/screens/nowPlaying/components/playBar/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginBottom: 5
+  },
+
+  slider: {
+    width: '85%'
+  },
+
+  thumb: {
+    width: 10,
+    height: 10,
+  }
+
+})
+
+export default styles;
