a48b5eef363011269cbdd7fe7007039af0e57f9c
Nycollas <nycollas.duaarte@hotmail.com>
2020-05-31 20:38:20 -0300

    Create styles.js

---
+++ src/screens/nowPlaying/components/header/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+    marginTop: 25,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 15
+  },
+
+  textPlaylistName: {
+    color: 'white'
+  },
+
+  icon: {
+    color: 'white'
+  }
+})
+
+export default styles;
