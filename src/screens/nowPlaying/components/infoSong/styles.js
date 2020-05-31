b437fc210ced4118f4031e882a4b9a4b720e07e2
Nycollas <nycollas.duaarte@hotmail.com>
2020-05-31 20:41:42 -0300

    Create styles.js

---
+++ src/screens/nowPlaying/components/infoSong/styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    alignItems: 'center'
+  },
+
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    
+  },
+  
+  descContainer: {
+    marginTop: 20,
+    alignItems: 'center'
+  },
+
+  musicName: {
+    color: 'white',
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+
+  autorName: {
+    marginTop: 5,
+    color: 'white',
+    fontSize: 14
+  }
+})
+
+export default styles;
