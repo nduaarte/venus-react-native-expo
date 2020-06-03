0f05eb67c363aac17f3cebe7bf00401c2a12c9cb
Nycollas <nycollas.duaarte@hotmail.com>
2020-06-02 23:09:12 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,36 +1,35 @@
+import  React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { createStackNavigator,  TransitionPresets } from '@react-navigation/stack';
+import SoundPlayer from 'react-native-sound-player';
 
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
+import NowPlaying from './src/screens/nowPlaying';
+import SongList from './src/screens/songList';
 
 const Stack = createStackNavigator();
 
-export default function App(props) {
-  const isLoadingComplete = useCachedResources();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='SongList'
+        screenOptions={{
+          headerShown: false
+        }}>
+        <Stack.Screen 
+          name='NowPlaying' 
+          component={NowPlaying}
+          options={{
+            ...TransitionPresets.ModalSlideFromBottomIOS,
+            gestureDirection: 'vertical'
+          }} 
+        />
+        <Stack.Screen 
+          name='SongList' 
+          component={SongList}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
