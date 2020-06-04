ca2935513f0a3b0d4e84c154e9c670ae2a15ab71
Nycollas <nycollas.duaarte@hotmail.com>
2020-06-04 20:51:52 -0300

    Update index.js

--- src/screens/nowPlaying/components/buttonsManager/index.js
+++ src/screens/nowPlaying/components/buttonsManager/index.js
@@ -1,36 +1,51 @@
-import React,{ useState, useEffect } from 'react';
+import React,{ Component } from 'react';
 import { View, TouchableOpacity } from 'react-native';
+import { Audio } from 'expo-av'
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import styles from './styles';
 
-export default function ButtonsManager() {
+export default class ButtonsManager extends Component {
+  async componentDidAmount() {
+    Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+      shouldDuckAndroid: true,
+      staysActiveInBackground: true,
+      playThroughEarpieceAndroid: true
+    });
 
-  const [on, setOn] = useState(false);
-  
-  useEffect(() => {
-    if(on == true) {
-      
-    }
-  }, [])
+    this.sound = new Audio.Sound();
 
-  function toggle() {
-    setOn(!on);
+    const status = {
+      shouldPlay: false
+    };
+
+    this.sound.loadAsync(require('../../../../musics/luxuria.mp3'), status, false);
   }
 
-  return(
-    <View style={styles.container}>
-      <TouchableOpacity>
-        <Icon style={styles.icon} size={35} name='ios-skip-backward'/>
-      </TouchableOpacity>
 
-      <TouchableOpacity style={styles.buttonContainer} onPress={() => toggle()}>
-        <Icon style={styles.icon} size={50} name={on ? 'ios-play' : 'ios-pause'} />
-      </TouchableOpacity>
+  playSound() {
+    this.sound.playAsync();
+  }
 
-      <TouchableOpacity>
-        <Icon style={styles.icon} size={35} name='ios-skip-forward'/>
-      </TouchableOpacity>
-    </View>
-  );
+  render() { 
+    return(
+      <View style={styles.container}>
+        <TouchableOpacity>
+          <Icon style={styles.icon} size={35} name='ios-skip-backward'/>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonContainer} onPress={this.playSound.bind(this)}>
+          <Icon style={styles.icon} size={50} name='ios-pause' />
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Icon style={styles.icon} size={35} name='ios-skip-forward'/>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
